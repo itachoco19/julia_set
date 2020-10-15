1fda8bf238635f523c3e34e5db1c7da3c33f0cef
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2020-10-15 18:13:17 +0900

    fix: エラーを修正

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv)
 {
-    double start_real = -1.0, start_image = -1.0, end_real = 1.0, end_image = 1.0, double interval = 0.01;
+    double start_real = -1.0, start_image = -1.0, end_real = 1.0, end_image = 1.0, interval = 0.01;
     char* julia_set = NULL;
     int real_count = ((int)end_real - (int)start_real) / 2;
     int image_count = ((int)end_image - (int)start_image) / 2;
