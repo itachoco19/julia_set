92f4ae18b8b0be9b165dfa98ee5865d43355a234
itachoco <b1615566@planet.kanazawa-it.ac.jp>
2020-11-25 00:07:18 +0900

    fix : 計算を打ち切らないように修正

--- julia.c
+++ julia.c
@@ -18,7 +18,11 @@ double is_divergence(double real, double image, double c_real, double c_image)
         double next_m_real = (m_real * m_real) - (m_image * m_image) + c_real;
         double next_m_image = (2.0 * m_real * m_image + c_image);
         //もし、発散するようならば、途中で計算を打ち切る
-        if((next_m_real * next_m_real + next_m_image * next_m_image > 4.0) && finish_flag) finish_flag = 0; result = iter;break;
+        if((next_m_real * next_m_real + next_m_image * next_m_image > 4.0) && finish_flag)
+        {
+            finish_flag = 0;
+            result = iter;
+        }
         m_real = next_m_real; m_image = next_m_image;
     }
     return ((double)(result)) / ((double)CONTINUE_TIME);
