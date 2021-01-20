abb5baff7d26fe1fb5449c66768ad86554371606
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2021-01-20 17:32:23 +0900

    fix : エラー分が一回だけ表示されるように修正

--- main.c
+++ main.c
@@ -9,7 +9,6 @@ int main(int argc, char** argv)
     int height = 0;
     if(argc < 3)
     {
-        fprintf(stderr, "use default 640x480\n");
         width = 640;
         height = 480;
     }
@@ -23,6 +22,10 @@ int main(int argc, char** argv)
     int id;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    if(id == 0)
+    {
+        fprintf(stderr, "use default 640x480\n");
+    }
     start = MPI_Wtime();
     //make_julia_set(width, height, -0.8, 0.15);
     make_julia_mpi(width, height, -0.8, 0.15, id);
